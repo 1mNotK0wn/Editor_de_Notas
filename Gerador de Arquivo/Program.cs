8a837c4c75454836b6268451a3474c82efbf4e5f
Guilherme T. Endo <guitoendo@gmail.com>
2023-09-07 12:36:23 -0300

    a

--- Gerador de Arquivo/Program.cs
+++ Gerador de Arquivo/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Security.Cryptography.X509Certificates;
 
 namespace GeradordeArquivo;
 
@@ -7,8 +8,11 @@ public class Program
 {
     const string ext = ".txt";
 
-    static void Main()
+    public static void Main()
     {
+
+        string file_path = Console.ReadLine() + "\\";
+
         while (true)
         {
             Console.Clear();
@@ -27,22 +31,22 @@ Selecione uma opção:
             {
                 case "1":
                     Console.Clear();
-                    CreateFile();
+                    CreateFile(file_path);
                     break;
 
                 case "2":
                     Console.Clear();
-                    ReadFile();
+                    ReadFile(file_path);
                     break;
 
                 case "3":
                     Console.Clear();
-                    OpenNote();
+                    OpenNote(file_path);
                     break;
 
                 case "4":
                     Console.Clear();
-                    DeleteNote();
+                    DeleteNote(file_path);
                     break;
 
                 case "5":
@@ -58,100 +62,101 @@ Selecione uma opção:
                     break;
             }
         }
-        
-    }
 
-    static void CreateFile()
-    {
-        Console.Write("Crie um título para a nota: ");
-        string name = Console.ReadLine();
-
-        string _file = name + ext;
-        EditText(_file);
-    }
-
-    static void ReadFile()
-    {
-        Console.WriteLine("Digite o título da nota que você quer ler: ");
-        string name = Console.ReadLine();
-        string pathing = file_path + name + ext;
-
-
-        if (File.Exists(pathing))
+        static void CreateFile(string file_path)
         {
-            var file = File.ReadAllText(pathing);
-            
-            Console.Clear();
-            Console.WriteLine(@$"
-{name}
+            Console.Write("Crie um título para a nota: ");
+            string name = Console.ReadLine();
 
-{file}");
-        }
-        else
-        {
-            Console.WriteLine($"O arquivo {name} não existe.");
+            string _file = name + ext;
+            EditText(_file, file_path);
         }
 
-        Console.ReadKey();
-    }
-
-    static void EditText(string _name)
-    {
-        StreamWriter stream_writer;
-
-        if (File.Exists(file_path + _name))
+        static void ReadFile(string file_path)
         {
-            Console.WriteLine($"O arquivo {_name} já existe.");
-        }
-        else
-        {
-            stream_writer = File.CreateText(file_path + _name);
-
-            Console.Clear();
-            Console.WriteLine("Nota criada com sucesso! Escreva o conteúdo da nota: ");
-
-            string content = Console.ReadLine();
-            stream_writer.Write(content);
-            stream_writer.Close();
-        }
-    }
-
-    static void OpenNote()
-    {
-        {
-            Console.Clear();
-            Console.WriteLine("Qual o título da nota que deseja abrir?");
-
+            Console.WriteLine("Digite o título da nota que você quer ler: ");
             string name = Console.ReadLine();
             string pathing = file_path + name + ext;
 
+
             if (File.Exists(pathing))
             {
-                System.Diagnostics.Process.Start("notepad", pathing);
+                var file = File.ReadAllText(pathing);
+
+                Console.Clear();
+                Console.WriteLine(@$"
+{name}
+
+{file}");
             }
             else
             {
                 Console.WriteLine($"O arquivo {name} não existe.");
-                Console.ReadKey();
             }
 
-        }
-    }
-
-    static void DeleteNote()
-    {
-        Console.Write("Digite o título da nota que deseja excluir: ");
-        string name = Console.ReadLine();
-
-        string path = file_path + name + ext;
-        if (File.Exists(path))
-        {
-            File.Delete(path);
-        }
-        else
-        {
-            Console.WriteLine($"O arquivo {name + ext} não existe e não pode ser deletado.");
             Console.ReadKey();
         }
+
+        static void EditText(string _name, string file_path)
+        {
+            StreamWriter stream_writer;
+
+            if (File.Exists(file_path + _name))
+            {
+                Console.WriteLine($"O arquivo {_name} já existe.");
+            }
+            else
+            {
+                stream_writer = File.CreateText(file_path + _name);
+
+                Console.Clear();
+                Console.WriteLine("Nota criada com sucesso! Escreva o conteúdo da nota: ");
+
+                string content = Console.ReadLine();
+                stream_writer.Write(content);
+                stream_writer.Close();
+            }
+        }
+
+        static void OpenNote(string file_path)
+        {
+            {
+                Console.Clear();
+                Console.WriteLine("Qual o título da nota que deseja abrir?");
+
+                string name = Console.ReadLine();
+                string pathing = file_path + name + ext;
+
+                if (File.Exists(pathing))
+                {
+                    System.Diagnostics.Process.Start("notepad", pathing);
+                }
+                else
+                {
+                    Console.WriteLine($"O arquivo {name} não existe.");
+                    Console.ReadKey();
+                }
+
+            }
+        }
+
+        static void DeleteNote(string file_path)
+        {
+            Console.WriteLine("Digite o título da nota que deseja excluir: ");
+            string name = Console.ReadLine();
+
+            string path = file_path + name + ext;
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+            }
+            else
+            {
+                Console.WriteLine($"O arquivo {name + ext} não existe e não pode ser deletado.");
+                Console.ReadKey();
+            }
+        }
+
     }
+    
 }
